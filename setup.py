0adf2b0a0f8969b8c419fb2da590cd9a177441e9
searri <searri98@gmail.com>
2022-02-28 15:35:34 -0500

    Setup gets dependencies from requirements.txt

--- setup.py
+++ setup.py
@@ -1,16 +1,23 @@
 import setuptools
+import os
+
+req_file = os.path.dirname(os.path.realpath(__file__)) + "/requirements.txt"
+if os.path.isfile(req_file):
+    with open(req_file, "r", encoding="utf8") as infile:
+        install_reqs = infile.read().splitlines()
 
 setuptools.setup(
     name="Online Group Modeling",
-    version="1.0.10",
+    version="1.1.0",
     author="Richard Sear",
     author_email="searri@gwu.edu",
     description="Package for using machine learning to analyze the behavior of online groups",
-    url="https://github.com/gwclusterlab/ogm",
+    url="https://github.com/gwdonlab/ogm",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires=">=3.6",
+    install_requires=install_reqs,
 )
